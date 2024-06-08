290a1016cb3e5d4e30f96c692e5e4cbc619fc6e7
Developer_Anil_Kumar <kum09a@gmail.com>
2024-06-08 10:58:29 +0530

    main category table create

---
+++ app/Models/backend/MainCategory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models\backend;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MainCategory extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "name",
+        "description",
+        "status"
+    ];
+}
